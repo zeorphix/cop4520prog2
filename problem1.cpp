2d40273014e852653f9e711e5c5636c83c17767c
zeorphix <tieue45@gmail.com>
2024-02-17 22:05:19 -0500

    create threads for each guest

--- problem1.cpp
+++ problem1.cpp
@@ -8,9 +8,26 @@
 
 const int NUM_GUESTS = 5;
 
+void guest(int id)
+{
+
+}
+
 int main(void)
 {
     using namespace std;
+
+    cout << "Preparing " << NUM_GUESTS << " guests to visit the labyrinth..." << endl;
+
+    thread guests[NUM_GUESTS];
+
+    for (int i = 0; i < NUM_GUESTS; ++i)
+        threads[i] = thread(guest, i + 1);
+
+    for (int i = 0; i < NUM_GUESTS; ++i)
+        threads[i].join();
+    
+    cout << "All guests have visited the labyrinth." << endl;
 
     return 0;
 }
